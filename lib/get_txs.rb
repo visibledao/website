2fea4ffe4f0ada2f802ade6a02d721b28d61b42a
Ryan Wold <wold@afomi.com>
2025-08-02 15:00:18 -0700

    push a github branch if data has been updated

--- lib/get_txs.rb
+++ lib/get_txs.rb
@@ -1,3 +1,4 @@
+require 'date'
 require_relative 'whats_on_chain.rb'
 
 # Get the Address balance from Whats on Chain
@@ -21,7 +22,40 @@ txs = txs.sort_by { |tx| tx["height"] }
 #   sleep 1.618
 # end
 
-puts "Saving _data/txs/index.json ..."
-File.open("_data/txs/index.json", "w") do |f|
-  f << JSON.pretty_generate(txs)
+json_path = "_data/txs/index.json"
+new_data = JSON.pretty_generate(txs)
+
+# 2. CREATE A BRANCH IF THERE ARE CHANGES ======================================
+
+# Read existing file if present
+existing_data = File.exist?(json_path) ? File.read(json_path) : ""
+
+# Check if data has changed
+if new_data == existing_data
+  puts "✅ No changes detected. Exiting..."
+  exit 0
 end
+
+# Write new data to file
+File.write(json_path, new_data)
+puts "Saved #{txs.size} transactions to #{json_path}"
+
+# Generate branch name
+date = Date.today.strftime("%Y-%m-%d")
+branch_name = "update-transactions-#{date}"
+
+# Set up Git
+# `git config --global user.email "github-actions@github.com"`
+# `git config --global user.name "GitHub Actions"`
+
+# Create new branch
+`git checkout -b #{branch_name}`
+
+# Commit the new data files
+`git add _data/txs/*`
+`git commit -m "Update transactions data from WhatsOnChain on #{date}"`
+
+# Push the branch to remote
+`git push origin #{branch_name}`
+
+puts "Pushed changes to branch #{branch_name}"
